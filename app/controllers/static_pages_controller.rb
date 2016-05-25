0474076a02c59116a7a9785cfdd1d99fed9bd207
Cody <codyhowell7@gmail.com>
2016-05-25 16:53:28 +0000

    Base Hl7 viewer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,7 @@ class StaticPagesController < ApplicationController
   
   def about 
   end
+  
+  def logic
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "HL7 Parser"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  def text_area
+    
+  end
 end
